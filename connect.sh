5e25da92870903d89571f1cfc51d802d5df65077
ffmammadov <farid.mammadov@pashabank.az>
2019-06-19 11:39:20 +0400

    Updated connect.sh

--- connect.sh
+++ connect.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-read -p "URL: " URL
+#This scripts try to send HEAD request to given URL and stops when it succeed 
+if [ -z $1 ]; then
+    read -p "URL: " URL
+else
+    URL=$1
+fi
 COUNTER=0
 while true; do
     ((COUNTER++))
     curl -s --head "$URL"
     RESPONSE_CODE=$?
     if [ $RESPONSE_CODE -eq 0 ]; then
-        echo "#Try no: ${COUNTER}: SUCCESS!"
+        echo "Try no: #${COUNTER}: SUCCESS!"
         break
     fi
-    echo "#Try no: ${COUNTER}: Connecting to ${URL} FAILED"
+    echo "Try no: #${COUNTER}: Connecting to ${URL} FAILED"
     sleep 5
 done
